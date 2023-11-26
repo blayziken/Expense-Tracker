256b097645872095b53f13426111923c317a4953
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2023-11-26 21:27:02 +0100

    Add new colours and create colour extension

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Blaze on 26/11/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let text = Color("Text")
+    static let icon = Color("Icon")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
